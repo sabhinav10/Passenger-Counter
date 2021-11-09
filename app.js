55ae03941b5b076ef52c7d82e88354b22148df09
sabhinav10 <sabhinav654@gmail.com>
2021-11-09 10:06:39 +0530

    setting count to zero

--- app.js
+++ app.js
@@ -10,4 +10,6 @@ let saveEl = document.getElementById("save-el");
 function save(){
     let countStr = count + ' - ';
     saveEl.textContent +=countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
