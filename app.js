9e7762a81746634a6c480cfb2c20796e3ac63f1c
sabhinav10 <sabhinav654@gmail.com>
2021-11-09 09:53:39 +0530

    created save button

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 function increment(){
     count = count + 1;
-    countEl.innerHTML = count;
+    countEl.innerText = count;
+}  
+
+let saveEl = document.getElementById("save-el");
+
+function save(){
+    let countStr = count + ' - ';
+    saveEl.textContent +=countStr;
 }
